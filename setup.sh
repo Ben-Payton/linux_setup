110c17ea8839e4392d09cd73e169eb3632518b5d
Ben <paytonb@proton.me>
2025-05-21 00:00:38 -0600

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,28 +1,51 @@
 #! /bin/bash
 
-echo "hello world"
+sudo apt dist-upgrade
+sudo apt install --upgrade
+sudo apt install --update
 
+# Gets zotero available to managed by apt
+wget -qO- https://raw.githubusercontent.com/retorquere/zotero-deb/master/install.sh | sudo bash
+sudo apt update
+
+echo ""
+echo ""
+echo "What is your name? (This will only be used for git):"
+read user_name
+echo "What is your email? (This will only be used for git and ssh keygen if ssh key does not already exist):"
+read user_email
 # make prefered directories
-mkdir ~/01_Projects
-mkdir ~/02_Areas
-mkdir ~/03_Resources
-mkdir ~/04_Archive
+
+function qdir {
+	if [ ! -d $1 ]; then
+		mkdir $1
+		echo "'$1' created."
+	else
+		echo "'$1' already exists."
+	fi
+}
+
+qdir ~/01_Projects
+qdir ~/02_Areas
+qdir ~/03_Resources
+qdir ~/04_Archive
 
 # install software i use regularly
-sudo apt install tmux git
+sudo apt install tmux git neovim neofetch zotero
 
 flatpak install flathub md.obsidian.Obsidian
 
 # Configure git to initialize the main branch as main instead of the outdated "master" branch
 git config --global init.defaultBranch main
-
+git config --global user.name $user_name
+git config --global user.email $user_email
 # Install and configure miniconda
 if [ ! -d ~/miniconda3 ]; then
 	wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 	bash Miniconda3-latest-Linux-x86_64.sh
 	rm Miniconda3-latest-Linux-x86_64.sh
 	source ~/.bashrc
-	conda create --name scienv python scipy numpy matplotlib pandas jupyter
+	conda create --name scienv python scipy numpy matplotlib pandas seaborn jupyter
 fi
 # move custom aliases as i like them
 if [ ! -f ~/.my_aliases ]; then
@@ -33,4 +56,13 @@ if [ ! -f ~/.my_aliases ]; then
 	echo "fi" >> ~/.bashrc
 	echo "#######" >> ~/.bashrc 
 	echo "" >> ~/.bashrc
+fi
+
+
+# generate an ssh key
+if [ ! -f ~/.ssh/id_ed25519.pub ]; then
+	echo "generating ssh key"
+	ssh-keygen -t ed25519 -C "$user_email"
+else
+	echo "ssh key already exits"
 fi
